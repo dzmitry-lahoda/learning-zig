0efb8054dfb396a4f2b0d80bcfd234b1c4996b1a
dzmitry-lahoda <dzmitry@lahoda.pro>
2020-07-22 19:07:12 +0300

    solved backtracking

--- backtracking/knight.zig
+++ backtracking/knight.zig
@@ -2,39 +2,97 @@
 const assert = @import("std").debug.assert;
 const fs = @import("std").fs;
 const std = @import("std");
-
-var board = [8][8]u8 {
-    [_]u8{0} ** 8,
-    [_]u8{0} ** 8,
-    [_]u8{0} ** 8,
-    [_]u8{0} ** 8,
-    [_]u8{0} ** 8,
-    [_]u8{0} ** 8,
-    [_]u8{0} ** 8,
-    [_]u8{0} ** 8
+var board = [8][8]i8 {
+    [_]i8{-1} ** 8,
+    [_]i8{-1} ** 8,
+    [_]i8{-1} ** 8,
+    [_]i8{-1} ** 8,
+    [_]i8{-1} ** 8,
+    [_]i8{-1} ** 8,
+    [_]i8{-1} ** 8,
+    [_]i8{-1} ** 8
 };
-
-
-var solution = [_]u32{0} ** 64;
-
-fn intToString(int: u32, buf: []u8) ![]const u8 {
-    return try std.fmt.bufPrint(buf, "{}", .{int});
+const coor = struct {
+    x:i8,
+    y:i8
+};
+pub fn solve_rec(pos:i8, c:coor) bool {
+    board[@intCast(usize, c.y)][@intCast(usize, c.x)] = pos;
+    if (pos >= 63) {
+        return true;
+    }
+    if (c.x + 2 < 8 and c.y + 1 < 8 and board[@intCast(usize, c.y + 1)][@intCast(usize, c.x + 2)] == -1){
+       var r =  solve_rec(pos+1, coor{.x = c.x+2, .y = c.y+1});
+       if (r){
+           return true;
+       }
+    }
+    if (c.x + 1 < 8 and c.y + 2 < 8 and board[@intCast(usize, c.y + 2)][@intCast(usize, c.x + 1)] == -1){
+        var r =  solve_rec(pos+1, coor{.x = c.x + 1, .y = c.y + 2});
+        if (r){
+           return true;
+       }
+    } 
+    if (c.x -1 > -1 and c.y + 2 < 8 and board[@intCast(usize, c.y + 2)][@intCast(usize, c.x - 1)] == -1){
+        var r =  solve_rec(pos+1, coor{.x = c.x -1,.y =  c.y + 2});
+        if (r){
+           return true;
+       }
+    }
+    if (c.x -2 > -1 and c.y + 1 < 8 and board[@intCast(usize, c.y + 1 )][@intCast(usize, c.x -2)] == -1){
+        var r =  solve_rec(pos+1, coor{.x = c.x - 2,.y =  c.y+1});
+        if (r){
+           return true;
+       }
+    } 
+     if (c.x -2 > -1 and c.y - 1  > -1 and board[@intCast(usize, c.y -1 )][@intCast(usize, c.x -2 )] == -1){
+        var r =  solve_rec(pos+1, coor{.x = c.x -2,.y =  c.y-1});
+        if (r){
+           return true;
+       }
+    } 
+    if (c.x -1  > -1 and c.y -2  > -1 and board[@intCast(usize, c.y -2 )][@intCast(usize, c.x -1 )] == -1){
+        var r =  solve_rec(pos+1, coor{.x = c.x-1, .y = c.y-2});
+        if (r){
+           return true;
+       }
+    } 
+     if (c.x + 1 < 8 and c.y -2  > -1 and board[@intCast(usize, c.y -2 )][@intCast(usize, c.x +1 )] == -1){
+        var r =  solve_rec(pos+1, coor{.x = c.x+1,.y =  c.y-2});
+        if (r){
+           return true;
+       }
+    } 
+    if (c.x + 2  < 8 and c.y - 1  > -1 and board[@intCast(usize, c.y -1 )][@intCast(usize, c.x + 2)] == -1){
+        var r =  solve_rec(pos+1, coor{.x = c.x+2, .y =  c.y-1});
+        if (r){
+           return true;
+       }
+    } 
+    if (pos == 0){
+        return false;
+    }
+        board[@intCast(usize, c.y)][@intCast(usize, c.x)] = -1;
+        return false;
+    
 }
 
 pub fn solve() !void {
-    var file = try fs.cwd().createFile("backtracking_knight.txt", .{});
-    
-
-    for (board) | row, i | {
-        //std.fmt.formatIntBuf(buffer[0..], )
-        for (row) | c, j | {
-            var buf = [_]u8{0}**8;
-            var wrote = try intToString(c, &buf);
-            //try std.fmt.bufPrint(buf, "{}", .{c});
-            var a_ = try file.write(wrote);
-        }
-        var a_ = try file.write("\n");
-    }  
+    var current:i8 = 0;
+    var n = coor{.x = 0, .y = 0};
+    if (solve_rec(0, n)) {
+        var file = try fs.cwd().createFile("backtracking_knight.txt", .{});
+        var pos:i8 = 1;
+        for (board) | row, i | {
+            for (row) | c, j | {
+                var buf = [_]u8{0}**8;
+                var wrote = try std.fmt.bufPrint(&buf, "  {:3}", .{c});
+                var a_ = try file.write(wrote);
+                //var b_ = try file.write(" ");
+            }
+            var a_ = try file.write("\n");
+        }  
+    }
 }
 
 test "solution" {
